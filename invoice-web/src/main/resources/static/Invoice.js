1b8f9a85e4c6b913f9b67f58e052dd621858df15
ashidak <ashidak@systena.co.jp>
2018-06-09 13:38:14 +0900

    spring bootの不要ファイルを削除

--- invoice-web/src/main/resources/static/Invoice.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-
-    $('#invoiceAppendBtn').on('click', function() {
-        alert("登録ゥ！！");
-    });
-    $('#invoiceSearchBtn').on('click', function() {
-        alert("検索ゥ！！");
-    });
-});

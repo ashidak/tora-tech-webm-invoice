3315f5c1e536501a587b9d604b0126400acd6ecd
ashidak <ashidak@systena.co.jp>
2018-06-23 03:09:59 +0900

    exceptionの追加

---
+++ invoice-api/src/main/java/invoice/invoice_api/model/exception/dateException.java
@@ -0,0 +1,39 @@
+package invoice.invoice_api.model.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+/**
+ * Instantiates a new date exception.
+ *
+ * @param startDate the start date
+ * @param endDate the end date
+ */
+@AllArgsConstructor
+
+/* (非 Javadoc)
+ * @see java.lang.Object#toString()
+ */
+@ToString
+
+/**
+ * Gets the invoice no.
+ *
+ * @return the invoice no
+ */
+
+/**
+ * Gets the end date.
+ *
+ * @return the end date
+ */
+@Getter
+public class dateException extends RuntimeException {
+
+    /** The start date. */
+    private String startDate;
+
+    /** The end date. */
+    private String endDate;
+}

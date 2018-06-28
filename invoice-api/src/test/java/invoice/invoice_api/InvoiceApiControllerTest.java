bebd5454196e60ae62f392f311457a7dfe22b4ea
ashidak <ashidak@systena.co.jp>
2018-06-28 23:38:46 +0900

    UIテストコードを作成

--- invoice-api/src/test/java/invoice/invoice_api/InvoiceApiControllerTest.java
+++ invoice-api/src/test/java/invoice/invoice_api/InvoiceApiControllerTest.java
@@ -1,50 +1,73 @@
 package invoice.invoice_api;
 
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import invoice.invoice_api.controller.InvoiceApiController;
+import invoice.invoice_api.model.response.dao.SearchInvoice;
 import invoice.invoice_api.model.response.entity.InvoiceError;
 import invoice.invoice_api.model.response.entity.InvoiceResult;
 import invoice.invoice_api.model.response.entity.ResponseInvoice;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(InvoiceApiApplication.class)
+@WebAppConfiguration
 public class InvoiceApiControllerTest {
+
+    @Rule
+    public final MockitoRule rule = MockitoJUnit.rule();
+
+    @InjectMocks
+    private InvoiceApiController controller;
+
+    @Mock
+    private SearchInvoice search;
 
     @Autowired
     private ObjectMapper mapper;
 
     private MockMvc mvc;
 
+    @Autowired
+    private HandlerExceptionResolver handlerExceptionResolver;
+
     @Before
     public void before() throws Exception {
-        mvc = MockMvcBuilders.standaloneSetup(new InvoiceApiApplication()).build();
+        mvc = MockMvcBuilders.standaloneSetup(controller)
+                .setHandlerExceptionResolvers(handlerExceptionResolver)
+                .build();
     }
 
-
     @Test
-    public void testGet__Ok() throws Exception {
-
-        Integer id = 26;
+    public void testGet_Ok() throws Exception {
 
         List<InvoiceResult> resultList = new ArrayList<InvoiceResult>();
         InvoiceResult result = new InvoiceResult();
 
-        result.setInvoiceNo(id.toString());
+        result.setInvoiceNo("26");
         result.setClientName("クライアント名");
         result.setClientAddress("顧客住所");
         result.setClientTel("03-1234-5678");
@@ -64,34 +87,37 @@ public class InvoiceApiControllerTest {
 
         resultList.add(result);
 
-        mvc.perform(MockMvcRequestBuilders.get("http://vhost.tora.com:8081/api/invoice/{id}", id))
-        .andExpect(status().isOk())
-        .andExpect(content().json(mapper.writeValueAsString(new ResponseInvoice(new ArrayList<InvoiceError>(), resultList))));
+        when(search.searchInvoice("26")).thenReturn(resultList);
+
+        mvc.perform(MockMvcRequestBuilders.get("/api/invoice/26"))
+        .andExpect(MockMvcResultMatchers.status().isOk())
+        .andExpect(content().json(
+                mapper.writeValueAsString(new ResponseInvoice(new ArrayList<InvoiceError>(), resultList))));
+
     }
 
     @Test
-    public void testGet__NG() throws Exception {
-
-        String id = "asd";
+    public void testGet_NG() throws Exception {
 
         List<InvoiceError> errorList = new ArrayList<InvoiceError>();
         InvoiceError error = new InvoiceError();
 
         error.setErrorCode("40001");
-        error.setErrorDetail("入力した請求書管理番号が不正です。");
-        error.setErrorMessage("Error. Input invoiceNo=\"asd\".");
+        error.setErrorMessage("入力した請求書管理番号が不正です。");
+        error.setErrorDetail("Error. Input invoiceNo=\"asd\".");
         errorList.add(error);
 
-        mvc.perform(MockMvcRequestBuilders.get("http://vhost.tora.com:8081/api/invoice/" + id))
+        mvc.perform(MockMvcRequestBuilders.get("/api/invoice/asd"))
         .andExpect(status().isBadRequest())
-        .andExpect(content().json(mapper.writeValueAsString(new ResponseInvoice(errorList, new ArrayList<InvoiceResult>()))));
+        .andExpect(content().json(
+                mapper.writeValueAsString(new ResponseInvoice(errorList, new ArrayList<InvoiceResult>()))));
     }
 
     @Test
-    public void testGet__Error() throws Exception {
+    public void testGet_Error() throws Exception {
 
-        mvc.perform(MockMvcRequestBuilders.get("http://vhost.tora.com:8081/api/invo"))
-        .andExpect(status().isInternalServerError());
+        mvc.perform(MockMvcRequestBuilders.get(""))
+        .andExpect(status().isNotFound());
     }
 
 }
